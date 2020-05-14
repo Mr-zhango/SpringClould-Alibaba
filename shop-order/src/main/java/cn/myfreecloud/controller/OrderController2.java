f8167d7fc933c560b3fad25d4c1c371ab884aa26
zhangyang <zhangyang@ec.e-chemchina.com>
2020-05-14 18:08:52 +0800

    服务雪崩的场景模拟

--- shop-order/src/main/java/cn/myfreecloud/controller/OrderController2.java
+++ shop-order/src/main/java/cn/myfreecloud/controller/OrderController2.java
@@ -7,23 +7,17 @@ import cn.myfreecloud.service.OrderService;
 import com.alibaba.fastjson.JSON;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.cloud.client.discovery.DiscoveryClient;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.client.RestTemplate;
+import org.springframework.web.bind.annotation.RestController;
 
 @Slf4j
-//@RestController
-public class OrderController {
+@RestController
+public class OrderController2 {
 
-    @Autowired
-    private RestTemplate restTemplate;
 
     @Autowired
     private OrderService orderService;
-
-    @Autowired
-    private DiscoveryClient discoveryClient;
 
     @Autowired
     private ProductService productService;
@@ -31,14 +25,11 @@ public class OrderController {
 
     // 下单
     @RequestMapping("/order/product/{pid}")
-    public Order order(@PathVariable("pid") Integer pid) {
+    public Order order(@PathVariable("pid") Integer pid) throws Exception {
         log.info("接收到{}号商品的下单请求", pid);
 
-        // List<ServiceInstance> instances = discoveryClient.getInstances("server-product");
-
-        // 使用ribbon实现客户端负载均衡
-        // 调用商品微服务
-        // Product product = restTemplate.getForObject("http://server-product/product/" + pid, Product.class);
+       // 模拟调用商品微服务需要2s时间
+        Thread.sleep(2000L);
 
         // 使用feign的形式调用
         Product product = productService.findByPid(pid);
@@ -53,11 +44,17 @@ public class OrderController {
         order.setPname(product.getPname());
         order.setPprice(product.getPprice());
         order.setNumber(1);
-        // 下单
-        orderService.createOrder(order);
+        // 防止下单出现垃圾数据
+        //orderService.createOrder(order);
 
-        log.info("创建订单成功,订单内容为{}", JSON.toJSONString(order));
+        //log.info("创建订单成功,订单内容为{}", JSON.toJSONString(order));
 
         return order;
+    }
+
+    // 高并发测试
+    @RequestMapping("/order/meaasge")
+    public String message() throws Exception {
+        return "高并发测试";
     }
 }

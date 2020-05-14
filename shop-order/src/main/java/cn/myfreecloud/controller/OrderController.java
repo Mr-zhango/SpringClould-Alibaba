5149c267184c820bac78aa1913011b243bcf9938
zhangyang <zhangyang@ec.e-chemchina.com>
2020-05-14 17:30:53 +0800

    使用ribbon实现客户端负载均衡

--- shop-order/src/main/java/cn/myfreecloud/controller/OrderController.java
+++ shop-order/src/main/java/cn/myfreecloud/controller/OrderController.java
@@ -6,14 +6,11 @@ import cn.myfreecloud.service.OrderService;
 import com.alibaba.fastjson.JSON;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.cloud.client.ServiceInstance;
 import org.springframework.cloud.client.discovery.DiscoveryClient;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
-
-import java.util.List;
 
 @Slf4j
 @RestController
@@ -34,15 +31,11 @@ public class OrderController {
     public Order order(@PathVariable("pid") Integer pid) {
         log.info("接收到{}号商品的下单请求", pid);
 
-        List<ServiceInstance> instances = discoveryClient.getInstances("server-product");
+        // List<ServiceInstance> instances = discoveryClient.getInstances("server-product");
 
-        ServiceInstance serviceInstance = instances.get(0);
-
-        String host = serviceInstance.getHost();
-        int port = serviceInstance.getPort();
-
+        // 使用ribbon实现客户端负载均衡
         // 调用商品微服务
-        Product product = restTemplate.getForObject("http://" + host + ":" + port + "/product/" + pid, Product.class);
+        Product product = restTemplate.getForObject("http://server-product/product/" + pid, Product.class);
 
         log.info("商品信息查询成功,内容为{}", pid, JSON.toJSONString(product));
 

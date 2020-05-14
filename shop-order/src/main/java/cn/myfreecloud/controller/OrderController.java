cfb550f22740a418fb14a5e542e9b9fecfe8bfea
zhangyang <zhangyang@ec.e-chemchina.com>
2020-05-14 17:21:50 +0800

    引入nacos管理微服务

--- shop-order/src/main/java/cn/myfreecloud/controller/OrderController.java
+++ shop-order/src/main/java/cn/myfreecloud/controller/OrderController.java
@@ -6,10 +6,14 @@ import cn.myfreecloud.service.OrderService;
 import com.alibaba.fastjson.JSON;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.client.ServiceInstance;
+import org.springframework.cloud.client.discovery.DiscoveryClient;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
+
+import java.util.List;
 
 @Slf4j
 @RestController
@@ -21,14 +25,24 @@ public class OrderController {
     @Autowired
     private OrderService orderService;
 
+    @Autowired
+    private DiscoveryClient discoveryClient;
+
 
     // 下单
     @RequestMapping("/order/product/{pid}")
     public Order order(@PathVariable("pid") Integer pid) {
         log.info("接收到{}号商品的下单请求", pid);
 
+        List<ServiceInstance> instances = discoveryClient.getInstances("server-product");
+
+        ServiceInstance serviceInstance = instances.get(0);
+
+        String host = serviceInstance.getHost();
+        int port = serviceInstance.getPort();
+
         // 调用商品微服务
-        Product product = restTemplate.getForObject("http://localhost:8081/product/" + pid, Product.class);
+        Product product = restTemplate.getForObject("http://" + host + ":" + port + "/product/" + pid, Product.class);
 
         log.info("商品信息查询成功,内容为{}", pid, JSON.toJSONString(product));
 
@@ -43,7 +57,7 @@ public class OrderController {
         // 下单
         orderService.createOrder(order);
 
-        log.info("创建订单成功,订单内容为{}",  JSON.toJSONString(order));
+        log.info("创建订单成功,订单内容为{}", JSON.toJSONString(order));
 
         return order;
     }

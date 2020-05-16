99feef8dac6da1b4f7944d0e940027800e9d76e7
zhangyang <zhangyang02@wanxue.cn>
2020-05-16 16:39:17 +0800

    nacos分环境配置

--- shop-product/src/main/java/cn/myfreecloud/controller/NacosConfigConfig.java
+++ shop-product/src/main/java/cn/myfreecloud/controller/NacosConfigConfig.java
@@ -15,6 +15,13 @@ public class NacosConfigConfig {
     @Value("${config.appName}")
     private String appName;
 
+    @Value("${config.env}")
+    private String env;
+
+    @Value("${config.test}")
+    private String test;
+
+
     @Autowired
     private ConfigurableApplicationContext applicationContext;
 
@@ -26,5 +33,15 @@ public class NacosConfigConfig {
     @RequestMapping("test-config2")
     public String testConfig2(){
         return appName;
+    }
+
+    @RequestMapping("test-config3")
+    public String testConfig3(){
+        return env;
+    }
+
+    @RequestMapping("test-config4")
+    public String testConfig4(){
+        return test;
     }
 }
